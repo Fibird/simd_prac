f98887d70884bf1ddb9f2192a5232a47bf906bf9
Liu Chaoyang <chaoyanglius@gmail.com>
2019-03-27 11:21:36 +0800

    improve project
    * modified print
    * add readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SIMD Practice
 
+Some simd practice codes.
+
+# Build
+
+You should use `-mavx` option after gcc:
+
+```
+gcc -mavx -O1 <source>.c -o <exe_name>
+```
--- src/simd_nlj_v2.c
+++ src/simd_nlj_v2.c
@@ -20,13 +20,13 @@ long run(int *data, int *vector, int n,int vec_len);
 int main()
 {
 	int * data=(int *)malloc(sizeof(int) * MAXELEMS);      /* foreign key column*/
-    printf("SIMD nest loop join(cycles)\n");
+    printf("SIMD nest loop join v2 (cycles)\n");
     printf("\t");
 	int step;
     for (step = 10; step <= 100; step+=10)
 	printf("%d%\t", step);
     printf("\n");
-	/*SIMD nest loop join for L1 cache sets*/
+	/*SIMD nest loop join v2 for L1 cache sets*/
 	printf("L1\t");
     for (step = 10; step <= 100; step+=10){
 		int i,vector_len=L1/4*step/100;
@@ -37,7 +37,7 @@ int main()
 		printf("%ld\t", run(data,vector,MAXELEMS, vector_len));
 	}
 	printf("\n");
-	/*SIMD nest loop join for L2 cache sets*/
+	/*SIMD nest loop join v2 for L2 cache sets*/
 	printf("L2\t");
     for (step = 10; step <= 100; step+=10){
 		int i,vector_len=L2/4*step/100;
@@ -48,7 +48,7 @@ int main()
 		printf("%ld\t", run(data,vector,MAXELEMS, vector_len));
 	}
 	printf("\n");
-	/*SIMD nest loop join for L3 cache sets*/
+	/*SIMD nest loop join v2 for L3 cache sets*/
 	printf("L3\t");
     for (step = 10; step <= 100; step+=10){
 		int i,vector_len=L3/4*step/100;
@@ -59,7 +59,7 @@ int main()
 		printf("%ld\t", run(data,vector,MAXELEMS,vector_len));
 	}
 	printf("\n");
-	/*SIMD nest loop join for LLC cache sets*/
+	/*SIMD nest loop join v2 for LLC cache sets*/
 	printf("LLC\t");
     for (step = 10; step <= 100; step+=10){
 		int i,vector_len=LLC/4*step/100;
@@ -85,8 +85,8 @@ void test(int *data, int *vector, int n, int vec_len) /* The test function */
     int result = 0; 
     volatile int sink=0; 
 	size_t nBlockWidth = 32 / (sizeof(float));
-	size_t cntBlock = n / (nBlockWidth);
-	size_t cntRem = n % (nBlockWidth);
+	size_t cntBlock = n / (nBlockWidth * 4);
+	size_t cntRem = n % (nBlockWidth * 4);
 
 	//printf("n=%d, vec_len=%d, cntBlock=%d, cntRem=%d\n", n, vec_len, cntBlock, cntRem);
 
@@ -94,8 +94,12 @@ void test(int *data, int *vector, int n, int vec_len) /* The test function */
 	int *p2 = data;
 	
     int *q = (int*)malloc(nBlockWidth * sizeof(int));    //将AVX变量上的多个数值合并时所用指针.
-	__m256 outVecs, innerVecs;
+	__m256 outVecs, outVecs1, outVecs2, outVecs3; 
+	__m256 innerVecs, innerVecs1, innerVecs2, innerVecs3;
+	// set rst to zero
 	__m256 cmpRst;	
+	__m256 rstSum = _mm256_setzero_ps();	
+
 	for (i = 0; i < vec_len; i++) {
 		for (k = 0; k < nBlockWidth; k++) {
 			p1[k] = i;
@@ -104,10 +108,26 @@ void test(int *data, int *vector, int n, int vec_len) /* The test function */
 		p2 = data;
 		for (j = 0; j < cntBlock; j++) {
 			innerVecs = _mm256_loadu_ps((float*)p2);
+			innerVecs1 = _mm256_loadu_ps((float*)p2 + nBlockWidth);
+			innerVecs2 = _mm256_loadu_ps((float*)p2 + nBlockWidth * 2);
+			innerVecs3 = _mm256_loadu_ps((float*)p2 + nBlockWidth * 3);
+
 			cmpRst = _mm256_cmp_ps(outVecs, innerVecs, _CMP_EQ_OQ);
 			_mm256_storeu_ps((float*)q, cmpRst);
             result += -(int)q[0] + -(int)q[1] + -(int)q[2] + -(int)q[3] + -(int)q[4] + -(int)q[5] + -(int)q[6] + -(int)q[7];
-			p2 += nBlockWidth;
+			
+			cmpRst = _mm256_cmp_ps(outVecs, innerVecs1, _CMP_EQ_OQ);
+			_mm256_storeu_ps((float*)q, cmpRst);
+            result += -(int)q[0] + -(int)q[1] + -(int)q[2] + -(int)q[3] + -(int)q[4] + -(int)q[5] + -(int)q[6] + -(int)q[7];
+
+			cmpRst = _mm256_cmp_ps(outVecs, innerVecs2, _CMP_EQ_OQ);
+			_mm256_storeu_ps((float*)q, cmpRst);
+            result += -(int)q[0] + -(int)q[1] + -(int)q[2] + -(int)q[3] + -(int)q[4] + -(int)q[5] + -(int)q[6] + -(int)q[7];
+
+			cmpRst = _mm256_cmp_ps(outVecs, innerVecs3, _CMP_EQ_OQ);
+			_mm256_storeu_ps((float*)q, cmpRst);
+            result += -(int)q[0] + -(int)q[1] + -(int)q[2] + -(int)q[3] + -(int)q[4] + -(int)q[5] + -(int)q[6] + -(int)q[7];
+			p2 += nBlockWidth * 4;
 		}			
 		for (s = 0; s < cntRem; s++) {
 			if (i == *p2)
@@ -117,7 +137,7 @@ void test(int *data, int *vector, int n, int vec_len) /* The test function */
 	}
 	
     sink = result; /* So compiler doesn't optimize away the loop */
-//	printf("[%ld]",sink);
+	//printf("[%ld]",sink);
 }
 
 long run(int *data, int *vector, int n, int vec_len)
